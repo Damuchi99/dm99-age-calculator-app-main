4065464de1eb0380f7f66875c095f198a54218c7
Damuchi99 <damuchi99@gmail.com>
2023-08-14 16:55:02 -0300

    Calculadora de edad finalizada, faltaría ajustar la versión de dispositivo movil.

--- js/age-calculator-app.js
+++ js/age-calculator-app.js
@@ -1,170 +1,131 @@
 const today = new Date();
 
+let actual_year = today.getFullYear();
+let actual_month = today.getMonth()+1;
+let actual_day = today.getDate();
+
+let day_title = document.getElementById('day-title');
+let month_title = document.getElementById('month-title');
+let year_title = document.getElementById('year-title');
+
+let day_input = document.getElementById('input-day');
+let month_input = document.getElementById('input-month');
+let year_input = document.getElementById('input-year');
+
+let texto_error_day = document.getElementById('texto-error-day');
+let texto_error_month = document.getElementById('texto-error-month');
+let texto_error_year = document.getElementById('texto-error-year');
+
+let fecha_correcta = true;
+let inputs_validos = true;
+let meses_con_30_dias = [4,6,9,11];
+
+let dd, mm, yyyy;
+
 document.getElementById('boton-calcular').addEventListener('click', function(){
-    let fecha_valida = validarFecha(today);
-
-    if (fecha_valida){
-        console.log("Se puede calcular edad");
-    }else{
-        console.log("Error al calcular edad");
-    }
-});
-
-function validarFecha(fecha){
-    let fecha_correcta = true;
-
     let day = document.getElementById('input-day').value;
     let month = document.getElementById('input-month').value;
     let year = document.getElementById('input-year').value;
 
-    /*console.log(month);
-    console.log(typeof(month));
-    console.log(typeof(parseInt(month)));
-    console.log(typeof(12));
-    console.log(parseInt(month) > 12);*/
-
-    let actual_year = fecha.getFullYear();
-
-    let day_title = document.getElementById('day-title');
-    let month_title = document.getElementById('month-title');
-    let year_title = document.getElementById('year-title');
-
-    let day_input = document.getElementById('input-day');
-    let month_input = document.getElementById('input-month');
-    let year_input = document.getElementById('input-year');
-
-    let texto_error_day = document.getElementById('texto-error-day');
-    let texto_error_month = document.getElementById('texto-error-month');
-    let texto_error_year = document.getElementById('texto-error-year');
-    
-    if(year > actual_year){
+    if(year == "" || year > actual_year){
+        fecha_correcta = false;
+        inputs_validos = false;
         year_title.style.color = "rgb(255, 87, 87)";
         year_input.style.borderColor = "rgb(255, 87, 87)";
-        texto_error_year.innerHTML = "Must be in the past";
-        fecha_correcta = false;
+        if(year == ""){
+            texto_error_year.innerHTML = "This field is required";
+        }else{
+            texto_error_year.innerHTML = "Must be in the past";
+        }
+    }else{
+        year_title.style.color = "rgb(113, 111, 111)";
+        year_input.style.borderColor = "rgb(113, 111, 111)";
+        texto_error_year.innerHTML = "";
     }
 
-    if(month < 1 || month > 12){
+    if(month == "" || month < 1 || month > 12){
+        fecha_correcta = false;
+        inputs_validos = false;
         month_title.style.color = "rgb(255, 87, 87)";
         month_input.style.borderColor = "rgb(255, 87, 87)";
-        texto_error_month.innerHTML = "Must be a valid month";
-        fecha_correcta = false;
+        if(month == ""){
+            texto_error_month.innerHTML = "This field is required";
+        }else{
+            texto_error_month.innerHTML = "Must be a valid month";
+        }
+    }else{
+        month_title.style.color = "rgb(113, 111, 111)";
+        month_input.style.borderColor = "rgb(113, 111, 111)";
+        texto_error_month.innerHTML = "";
     }
 
-    if(day < 1 || day > 31){
-        day_title.style.color = "rgb(255, 87, 87)";
-        day_input.style.borderColor = "rgb(255, 87, 87)";
-        texto_error_day.innerHTML = "Must be a valid day";
+    if(day == "" || day < 1 || day > 31){
         fecha_correcta = false;
-    }/*else{
+        inputs_validos = false;
         day_title.style.color = "rgb(255, 87, 87)";
         day_input.style.borderColor = "rgb(255, 87, 87)";
+        if(day == ""){
+            texto_error_day.innerHTML = "This field is required";
+        }else{
+            texto_error_day.innerHTML = "Must be a valid day";
+        }
+    }else{
+        day_title.style.color = "rgb(113, 111, 111)";
+        day_input.style.borderColor = "rgb(113, 111, 111)";
         texto_error_day.innerHTML = "";
-    }*/
-
-    console.log(day_title.style.color.value);
-    console.log((day_title.style.color == "rgb(113, 111, 111)"));
-    console.log(texto_error_day.innerHTML == "");
-
-    if(inputsValidos(texto_error_day, texto_error_month, texto_error_year) && !fechaCorrecta(day, month, year)){
-        year_title.style.color = "rgb(255, 87, 87)";
-        year_input.style.borderColor = "rgb(255, 87, 87)";
-        month_title.style.color = "rgb(255, 87, 87)";
-        month_input.style.borderColor = "rgb(255, 87, 87)";
-        day_title.style.color = "rgb(255, 87, 87)";
-        day_input.style.borderColor = "rgb(255, 87, 87)";
-        texto_error_day.innerHTML = "Must be a valid date";
-
-        fecha_correcta = false;
     }
 
-    return fecha_correcta;
+    if(inputs_validos){
+        if((month==2 && (day>29 || (day==29 && year%4!=0))) || (meses_con_30_dias.includes(parseInt(month)) && day==31)){
+            fecha_correcta = false;
+            day_title.style.color = "rgb(255, 87, 87)";
+            day_input.style.borderColor = "rgb(255, 87, 87)";
+            month_title.style.color = "rgb(255, 87, 87)";
+            month_input.style.borderColor = "rgb(255, 87, 87)";
+            year_title.style.color = "rgb(255, 87, 87)";
+            year_input.style.borderColor = "rgb(255, 87, 87)";
+            texto_error_day.innerHTML = "Must be a valid date";
+        }else if(year == actual_year){
+            if(month > actual_month){
+                fecha_correcta = false;
+                month_title.style.color = "rgb(255, 87, 87)";
+                month_input.style.borderColor = "rgb(255, 87, 87)";
+                texto_error_month.innerHTML = "Must be in the past";
+            }else if (month == actual_month && day > actual_day){
+                fecha_correcta = false;
+                day_title.style.color = "rgb(255, 87, 87)";
+                day_input.style.borderColor = "rgb(255, 87, 87)";
+                texto_error_day.innerHTML = "Must be in the past";
+            }
+        }
+    }
+
+    if(fecha_correcta){
+        yyyy = actual_year - year;
+
+        if(actual_month >= month){
+            mm = actual_month - month;
+        }else{
+            yyyy--;
+            mm = 12 + actual_month - month;
+        }
+
+        if(actual_day >= day){
+            dd = actual_day - day;
+        }else{
+            mm--;
+            dd = getDaysInMonth(year, month) + actual_day - day;
+        }
+
+        document.getElementById('years').innerHTML = yyyy;
+        document.getElementById('months').innerHTML = mm;
+        document.getElementById('days').innerHTML = dd;
+    }
+
+    fecha_correcta = true;
+    inputs_validos = true;
+});
+
+function getDaysInMonth(anio, mes){
+    return new Date(anio, mes, 0).getDate();
 }
-
-function inputsValidos(texto_error_dia, texto_error_mes, texto_error_anio){
-    return texto_error_dia.innerHTML == "" &&
-           texto_error_mes.innerHTML == "" &&
-           texto_error_anio.innerHTML == "";
-}
-
-function fechaCorrecta(dia,mes,anio){
-    let meses_con_30_dias = [4,6,9,11];
-
-    return (mes==2 && (dia<29 || (dia==29 && anio%4==0))) || 
-           (meses_con_30_dias.includes(mes) && dia!=31) ||
-           (!meses_con_30_dias.includes(mes));
-}
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-/*if(year > actual_year){
-    cambiarEstiloFormularioError(year_title, year_input);
-    texto_error_year.innerHTML = "Must be in the past";
-}else{
-    cambiarEstiloFormularioNormal(year_title, year_input);
-    texto_error_day.innerHTML = "";
-}
-
-if(month < 1 || month > 12){
-    cambiarEstiloFormularioError(month_title, month_input);
-    texto_error_month.innerHTML = "Must be a valid month";
-}else{
-    cambiarEstiloFormularioNormal(month_title, month_input);
-    texto_error_day.innerHTML = "";
-}
-
-if(day < 1 || day > 31){
-    cambiarEstiloFormularioError(day_title, day_input);
-    texto_error_day.innerHTML = "Must be a valid day";
-}else{
-    cambiarEstiloFormularioNormal(day_title, day_input);
-    texto_error_day.innerHTML = "";
-}
-
-if(datosValidos(day_title, month_title, year_title) && !fechaCorrecta(day,month,year)){
-    cambiarEstiloFormularioError(year_title, year_input);
-    cambiarEstiloFormularioError(month_title, month_input);
-    cambiarEstiloFormularioError(day_title, day_input);
-    texto_error_day.innerHTML = "Must be a valid date";
-    fecha_correcta = false;
-}else{
-    cambiarEstiloFormularioNormal(year_title, year_input)
-    cambiarEstiloFormularioNormal(month_title, month_input);
-    cambiarEstiloFormularioNormal(day_title, day_input);
-    texto_error_day.innerHTML = "";
-}
-
-return fecha_correcta;
-}
-
-function cambiarEstiloFormularioError(label, input){
-label.style.color = "rgb(255, 87, 87)";
-input.style.borderColor = "rgb(255, 87, 87)";
-}
-
-function cambiarEstiloFormularioNormal(label, input){
-label.style.color = "black";
-input.style.borderColor = "black";
-}*/
